736839ff4e62595befb8df2eb63eda6187b1d70d
Mariana Rios Silveira Carvalho <marianariossc@gmail.com>
2017-09-22 23:51:10 -0300

    Update
    A REST API calls a JSON and display the data in the TableView.

---
+++ Contacts/Controller/ContactTableViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ContactTableViewController.swift
+//  Contacts
+//
+//  Created by Mariana Rios Silveira Carvalho on 21/09/17.
+//  Copyright © 2017 Mariana Rios Silveira Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewController: UITableViewController, URLSessionDataDelegate {
+
+    var jsonData = Data()
+    var users =  [User]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let cfg = URLSessionConfiguration.default
+        cfg.allowsCellularAccess = true
+        cfg.networkServiceType = .default
+        cfg.requestCachePolicy = .returnCacheDataElseLoad
+        cfg.isDiscretionary = true
+        cfg.urlCache = URLCache(memoryCapacity: 0, diskCapacity: 10, diskPath: NSTemporaryDirectory())
+
+        let queue = OperationQueue()
+        queue.qualityOfService = .userInteractive
+        queue.maxConcurrentOperationCount = 5
+        queue.underlyingQueue = DispatchQueue.global(qos: .userInteractive)
+        
+        let session = URLSession(configuration: cfg, delegate: self, delegateQueue: queue)
+        
+        if let url = URL(string: "https://jsonplaceholder.typicode.com/users") {
+            var request = URLRequest(url:url)
+            request.timeoutInterval = 10
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            
+            let dataTask = session.dataTask(with: request)
+            dataTask.resume()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.username
+        
+        return cell
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        jsonData.append(data)
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        do {
+            let decoder = JSONDecoder()
+            users = try decoder.decode([User].self, from: jsonData)
+            
+            DispatchQueue.main.async { [unowned self] in
+                self.tableView.reloadData()
+            }
+        }catch {
+            debugPrint(error)
+        }
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Contacts/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Contacts
+//
+//  Created by Mariana Rios Silveira Carvalho on 21/09/17.
+//  Copyright © 2017 Mariana Rios Silveira Carvalho. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    let id: Int
+    let name: String
+    let username: String
+    
+}
